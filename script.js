9122e671043bdda6e59f267d01572dd271ae9c59
Matteo Pompei <datxizzan@gmail.com>
2021-12-10 16:52:46 +0100

    Fizz, Buzz e FizzBuzz aggiunti

--- script.js
+++ script.js
@@ -1,3 +1,13 @@
 for (let i = 1; i <= 100; i++) { // Genera numeri da 1 a 100
-  console.log (i); // Scrive i numeri in console
+
+  if((i%3 == 0) && (i%5 == 0)){ // Stampa FizzBuzz se multiplo di 3 e 5
+    console.log ("FizzBuzz");
+  } else if((i%3 == 0)) { // Stampa Fizz se multipplo di 3
+    console.log ("Fizz");
+  } else if ((i%5 == 0)) { // Stampa Buzz se multipplo di 5
+    console.log ("Buzz");
+  } else {
+    console.log (i); // Scrive i numeri in console
+}
+
 }
